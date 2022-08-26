048a726b62440ab111cf7f4d1da5156fac1410a8
Theophileaseh <theophilenwachan@gmail.com>
2022-08-26 16:15:41 +0100

    Fix linter errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,13 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025} ActionMailer::Base.delivery_method = :smtp ActionMailer::Base.smtp_settings = { :tls => true, :address => "smtp.gmail.com", :port => 587, :domain => "gmail.com", :authentication => :login, :user_name => "[username]", :password => "[password]" } 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025} 
+      ActionMailer::Base.delivery_method = :smtp 
+      ActionMailer::Base.smtp_settings = { :tls => true, :address => "smtp.gmail.com", 
+                                           :port => 587, :domain => "gmail.com", 
+                                           :authentication => :login, :user_name => "[username]", 
+                                           :password => "[password]" } 
 
   
   # In the development environment your application's code is reloaded any time
